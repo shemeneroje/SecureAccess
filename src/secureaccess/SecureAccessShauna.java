f35ae4bfc9492882bc8b9cb7a800658836a05ed9
Shemen Eroje <seroje777@gmail.com>
2025-10-23 14:36:57 +0100

    updated project

---
+++ src/secureaccess/SecureAccessShauna.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package secureaccess;
+
+/**
+ *
+ * @author shaun
+ */
+public class SecureAccessShauna {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        welcomeGUI myGUI = new welcomeGUI(); 
+        myGUI.setVisible(true);
+    }
+    
+}
